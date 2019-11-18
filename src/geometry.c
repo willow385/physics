99203e829577b77ecaf64c167b6e6f40f0e68370
Dante Falzone <dante.j.falzone@protonmail.com>
2019-11-18 14:54:45 -0600

    did a lil refactoring and made it more typesafe

--- src/geometry.c
+++ src/geometry.c
@@ -3,19 +3,15 @@
 #include <math.h>
 
 int is_distance(
-    int x_pos_0,
-    int y_pos_0,
-    int x_pos_1,
-    int y_pos_1,
+    int horiz_dist,
+    int vert_dist,
     float distance
 ) {
     // We will compute this with the pythagorean theorem.
-    int horiz_dist = x_pos_1 - x_pos_0;
-    int vert_dist = y_pos_1 - y_pos_0;
-    float points_dist = sqrt(
-        pow((float)horiz_dist, 2.0)
-        + pow((float)vert_dist, 2.0)
+    int points_dist = (
+        pow(horiz_dist, 2)
+        + pow(vert_dist, 2)
     );
 
-    return (points_dist <= distance);
+    return (points_dist <= pow(distance, 2));
 }
--- src/geometry.h
+++ src/geometry.h
@@ -1,16 +1,12 @@
 /**
  * Function to tell if two points are a given distance apart.
- * @param x_pos_0 the x-position of the first point
- * @param y_pos_0 the y-position of the first point
- * @param x_pos_1 the x-position of the second point
- * @param y_pos_1 the y-position of the second point
+ * @param horiz_dist the difference between the points' x-coords
+ * @param vert_dist the difference between the points' y-coords
  * @param distance how far apart we want to know if they are
  * @return 1 if points are approximately [distance] pixels apart, else 0
  */
 int is_distance(
-    int x_pos_0,
-    int y_pos_0,
-    int x_pos_1,
-    int y_pos_1,
+    int horiz_dist,
+    int vert_dist,
     float distance
 );
